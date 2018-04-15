c05f9832df4cdd7ead0f121c88e499797a2c7da7
dyoshikawa <dyoshikawa1993@gmail.com>
2018-04-15 20:47:20 +0900

    fix dockerイメージのdevをlatestに

--- first-boot.sh
+++ first-boot.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --rm -v `pwd`/:/app dyoshikawa/laravel:dev bash -c "composer install && cp -p -f .env.devkit .env && php artisan key:generate"
+docker run --rm -v `pwd`/:/app dyoshikawa/laravel:latest bash -c "composer install && cp -p -f .env.devkit .env && php artisan key:generate"
