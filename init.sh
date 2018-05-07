f00c0905940d88616aad7ca2f8b9a718162cf68e
dyoshikawa <yoshikawada@crunchtimer.jp>
2018-05-07 19:53:36 +0900

    fix init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,46 @@
 #!/usr/bin/env bash
-docker run --rm -v `pwd`/:/app dyoshikawa/laravel:latest bash -c "composer install && cp -p -f .env.devkit .env && php artisan key:generate"
+
+echo "Initialized start"
+
+if [ ! -f composer.json ]; then
+
+    echo "delete files"
+
+    path=`ls -a`;
+    declare -a exists=();
+    exists=${path//\// };
+
+    for e in ${exists[@]};
+    do
+        case $e in
+            ".")
+                echo "skip .";;
+            "..")
+                echo "skip ..";;
+            "init.sh")
+                echo "skip init.sh";;
+            "docker-compose.yml")
+                echo "skip docker-compose.yml";;
+            ".idea")
+                echo "skip .idea";;
+            *)
+                rm -rf $e
+                echo "delete" $e
+        esac
+    done
+
+    echo "generate laravel files"
+
+    docker run -d --name dyoshikawa-laravel dyoshikawa/laravel
+    docker cp dyoshikawa-laravel:/app ./laravel-app
+    mv laravel-app/* .
+    mv laravel-app/.??* .
+    rmdir laravel-app
+fi
+
+docker run --rm -v `pwd`/:/app dyoshikawa/laravel:latest bash -c \
+    "composer install && cp -p -f .env.devkit .env && php artisan key:generate"
+
+echo "Initialized successfully!"
+
+exit 0
