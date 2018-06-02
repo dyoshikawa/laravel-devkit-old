455034d49edfc76ffbacc695b44f951849564d75
dyoshikawa <dyoshikawa1993@gmail.com>
2018-06-02 16:03:49 +0900

    fix init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-docker run --rm -v `pwd`:/myproject dyoshikawa/laravel composer install
+docker run --rm -v `pwd`:/myproject dyoshikawa/laravel composer update
 cp -p .env.devkit .env
 docker run --rm -v `pwd`:/myproject dyoshikawa/laravel php artisan key:generate
-docker run --rm -v `pwd`:/myproject dyoshikawa/laravel php artisan ide-helper:generate
-docker run --rm -v `pwd`:/myproject dyoshikawa/laravel php artisan ide-helper:meta
